2395dc70349ae3e7250acbf3778423b9fb930440
kentaro.yamasaki <kentaro.yamasaki@72-49542>
2024-12-23 14:13:27 +0900

    プロジェクト ファイルを追加します。

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace HelloWorld1
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.ReadOnly)]
+    public class Class1 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elementSet)
+        {
+            TaskDialog.Show("Hello", "Hello World");
+            return Result.Succeeded;
+        }
+    }
+}
